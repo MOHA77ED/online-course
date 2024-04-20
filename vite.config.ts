e3df4e8c53c5aeaf3c7d87404e156e53f30a035e
Mohammed Naji <mna816008@gmail.com>
2024-04-20 20:49:25 +0300

    upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/online-course/"
-})
+	plugins: [react()],
+	base: "/online-course/",
+});
